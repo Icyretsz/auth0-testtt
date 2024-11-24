e41e7e12724171314db0889d0807275cf28582d1
IcyretszMacbookPro16 <ubw1212@gmail.com>
2024-11-25 02:53:22 +0700

    setting up

--- app/error/page.tsx
+++ app/error/page.tsx
@@ -1,12 +1,12 @@
 'use client'
 
 import Link from "next/link";
-import {useSearchParams} from "next/navigation";
+import {useParams} from "next/navigation";
 
 
 const Page = () => {
-    const searchParams = useSearchParams();
-    const message = searchParams.get('message') || 'An unexpected error occurred. Please try again.';
+    const searchParams = useParams();
+    const message = searchParams.message;
     console.log('message:',message);
 
     if (message && message.includes('access-denied')) {
