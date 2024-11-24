abcbcbe507b8795c93851a01c5c33237245b9569
IcyretszMacbookPro16 <ubw1212@gmail.com>
2024-11-25 00:03:38 +0700

    setting up

--- app/[subdomain]/page.tsx
+++ app/[subdomain]/page.tsx
@@ -1,18 +1,21 @@
 'use client'
-
+import React from 'react';
 import {useUser} from "@auth0/nextjs-auth0/client";
-
+import {useParams} from "next/navigation";
 
 const Page = () => {
+
+  const params = useParams();
+  const orgName = params.subdomain;
 
   const { user } = useUser()
 
   return (
-    <div>
-      Welcome {user?.name}
-        {/* eslint-disable-next-line @next/next/no-html-link-for-pages */}
-        <a href="/api/auth/logout">Logout</a>
-    </div>
+      <div>
+        <h1 style={{fontSize: 50}}>This is {orgName} page</h1>
+        Welcome {user?.name}
+        <div><a href='/api/auth/logout'>Logout</a></div>
+      </div>
   );
 };
 
